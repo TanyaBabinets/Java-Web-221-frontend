724157760864bf2910603f6bc9cecfdd21b51fdd
TanyaBabinets <tanya060616@gmail.com>
2025-02-25 20:23:26 +0200

    int age added

--- src/App.jsx
+++ src/App.jsx
@@ -1,164 +1,57 @@
-import { useState } from 'react'
-
 import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Home    from './views/Home/Home';
+import Profile from './views/Profile/Profile';
+import Signin  from './views/Signin/Signin';
+import Signup  from './views/Signup/Signup';
 import './style.css'
+import AppContext from './AppContext';
+
+
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
 
-  const [name, setName] = useState("");
-  const [login, setLogin] = useState("");
-  const [emails, setEmails] = useState([""]);
-  const [phones, setPhones] = useState([""]);
-  const [regdate, setRegdate] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordC, setPasswordC] = useState("");
-  const [city, setCity] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [emailErrors, setEmailErrors] = useState([]);
-
-  const addEmail = () => {
-    setEmails([...emails, ""])
-  }
-  const addPhone = () => {
-    setPhones([...phones, ""])
-  }
-  const emailChange = (e, index) => {
-    const updateEmails = [...emails];
-    updateEmails[index] = e.target.value;
-    setEmails(updateEmails);
-  }
-
-
-  const phoneChange = (e, index) => {
-    const updatePhones = [...phones];
-    updatePhones[index] = e.target.value;
-    setPhones(updatePhones);
-  }
-  //проверка одного емейла
-  const checkEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-  //проверка массива
-  const checkEmails = (emails) => {
-    const errors = emails.map((email, index) => {
-      if (!checkEmail(email)) {
-        return `Email ${index + 1} has wrong format`;
-      }
-      return null;
-    });
-    return errors.filter(error => error !== null);
-  }
-
-
-  const checkPassword = () => {
-    const passwordRegex = /^(?=.*[A-Z]).{5,}$/; // Не менее 5 символов, 1 заглавная буква
-    if (!passwordRegex.test(password)) {
-      setErrorMessage(
-        "Password must be at least 5 characters long and contain at least one capital letter."
-      );
-      return false;
-    }
-    if (password !== passwordC) {
-      setErrorMessage("Passwords do not match.");
-      return false;
-    }
-    return true;
-  };
-
-
-
-  const sendForm = () => {
-    const emailErrors = checkEmails(emails);
-    if (emailErrors.length > 0) {
-      setEmailErrors(emailErrors);
-      return;
-    }
-    if (!checkPassword()) {
-      return;
-    }
-
-    const data = {
-      name, login, emails: emails.join(", "), phones: phones.join(", "), regdate, password, city
-    };
-    console.log(data);
-    fetch("http://localhost:8081/javaweb221/home", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    }
-    )
-      .then(r => r.json())
-      .then(j => {
-        console.log(j);
-      });
-  }
-  return (
-    <>
-      <input value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="Enter your name" />
-      <br />
-      <input value={login}
-        onChange={e => setLogin(e.target.value)}
-        placeholder="Enter your login" />
-      <br />
-
-
-
-      {emails.map((email, index) => (
-        <div>
-          <input key={index} type='email'
-            value={email}
-            className="input-email"
-            onChange={(e) => emailChange(e, index)}
-            placeholder="Enter your email" />
-          {emailErrors[index] &&
-            (
-              <p style={{ color: "red" }}>{emailErrors[index]}</p>
-            )}
-
-        </div>
-      ))}
-
-      <br />
-      <button onClick={addEmail}>Add additional email</button>
-
-      <br />
-      {phones.map((phone, index) => (
-        <input key={index} type='text'
-          value={phone}
-          className="input-email"
-          onChange={(e) => phoneChange(e, index)}
-          placeholder="Enter your phone" />
-      ))}
-      <br />
-      <button onClick={addPhone}>Add additional Phone</button>
-      <br />
-      <input type='date' value={regdate}
-        onChange={e => setRegdate(e.target.value)}
-        placeholder="Registration date" />
-      <br />
-      <input type='password' value={password}
-        onChange={e => setPassword(e.target.value)}
-        placeholder="Enter your password" />
-      <br />
-      <input type='password' value={passwordC}
-        onChange={e => setPasswordC(e.target.value)}
-        placeholder="Confirm your password" />
-      <br />
-      <input type='text' value={city}
-        onChange={e => setCity(e.target.value)}
-        placeholder="Enter your city" />
-      <br />
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      <button onClick={sendForm}>Register</button>
-
-
-    </>
-  )
+  return <AppContext.Provider value={{user, setUser, request, accessToken, setAccessToken}}> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
+  </AppContext.Provider>;
 }
+
+const request = (url, conf) => new Promise( (resolve, reject) => {
+  const backHost = "http://localhost:8082/javaweb221";
+  ///если в контексте есть токен, то 
+  if (accessToken && accessToken.accessTokenId) { 
+    if (!conf) { 
+      conf = {};
+    }
+    if (!conf.header) { 
+      conf.headers = {}
+    }
+    if (!conf.header["Authorization"]) {
+      conf.headers["Authorization"] = "Bearer " + accessToken.accessTokenId;
+    }
+
+  }
+  fetch(backHost + url, conf)
+  .then( r=> r.json() )
+  .then( j => {
+    if( j.status < 300 ) {
+      resolve( j.data );
+    }
+    else {
+      reject( j );
+    }
+  })
+  .catch( reject );
+});
 
 export default App
---
+++ src/views/Signin/Signin.jsx
@@ -0,0 +1,43 @@
+import { useContext, useState } from 'react';
+import AppContext from '../../AppContext';
+import { useNavigate } from 'react-router-dom';
+
+export default function Signin() {    
+    const [email, setEmail] = useState("user@i.ua");
+    const [password, setPassword] = useState("123");
+    const {setAccessToken, setUser, request} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const sendForm = () => {
+        // rfc7617
+        let data = email + ':' + password;
+        let credentials = btoa(data);
+        request('/user', {
+          method: 'GET',
+          headers: {
+            'Authorization': 'Basic ' + credentials,
+          },
+        }).then( data => {
+          setUser(data.user);
+          setAccessToken(data.accessToken);
+            navigate('/profile');
+        }).catch( console.log );
+    }
+
+    return <form>
+        <input 
+            type='email'
+            value={email} 
+            onChange={e => setEmail(e.target.value)}
+            placeholder="E-mail" />
+          <br/>
+          <input 
+            type='password'
+            value={password} 
+            onChange={e => setPassword(e.target.value)}
+            placeholder="*********" />
+          <br/>
+          <button type='button' onClick={sendForm}>Вхід</button>
+          
+    </form>;
+}
